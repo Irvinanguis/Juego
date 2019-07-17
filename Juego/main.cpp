5a2ec2202afd96dff27aebfe4d9be4b2503e3c60
Irvin Anguis Quispe <ianguisq@icloud.com>
2019-07-17 11:52:44 -0500

    Initial Commit

---
+++ Juego/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Juego
+//
+//  Created by Irvin Paul Anguis Quispe on 7/17/19.
+//  Copyright © 2019 Irvin Paul Anguis Quispe. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
